c7645c7f34438aa7ad0565d58dcc08e5a94409dd
kristis <kristisappdev@gmail.com>
2025-09-07 17:45:40 +0300

    Add printing all permutations of transposition ciphertext

--- lab01/3-transposition.py
+++ lab01/3-transposition.py
@@ -1,18 +1,14 @@
-text = (
-    ["1 ŠANTVAYAIOPAASAKDŲ"],
-    ["2 LIIĄŠŠAŠNIIGASSAEK"],
-    ["3 IIEEŠTTSSIRDRUTITS"],
-    ["4 KUĄIYUNIVJARŽIKIEI"],
-    ["5 ĖVRMKKTIYOSĖIAAPBŠ"]
-)
+import itertools
 
-length = len(text)
+text = [
+    "1 ŠANTVAYAIOPAASAKDŲ",
+    "2 LIIĄŠŠAŠNIIGASSAEK",
+    "3 IIEEŠTTSSIRDRUTITS",
+    "4 KUĄIYUNIVJARŽIKIEI",
+    "5 ĖVRMKKTIYOSĖIAAPBŠ"
+]
 
-print(length)
-
-for index, line in enumerate(text): 
-    for i in range(length):
-        print(text[i])
-        for j in range(index + 1, length - 1):
-            print(text[j])
-    print('\n')
+for perm in itertools.permutations(text):
+    for line in perm:
+        print(line)
+    print()
