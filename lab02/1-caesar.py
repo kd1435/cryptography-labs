c5f563ce2473856f536abb694f89978c117c5229
kristis <kristupas.dansevicius@mif.stud.vu.lt>
2025-09-13 11:14:55 +0300

    Move up the condition for gcd in the loop

--- lab02/1-caesar.py
+++ lab02/1-caesar.py
@@ -69,10 +69,10 @@ beginning_of_plaintext = 'KIE'
 possible_keys = []
 beginning_of_ciphertext = ciphertext[:len(beginning_of_plaintext)]
 #print (beginning_of_ciphertext)
+# TODO: hint: don't forget to check the additional condition for k1: gcd(k1, n) == 1
 for key in range(n): # from 0 to n-1      # TODO: two loops - for k1 and for k2
-    if decryptCaesar (beginning_of_ciphertext, key) == beginning_of_plaintext: 
-        # TODO: hint: don't forget to check the additional condition for k1: gcd(k1, n) == 1
-        if math.gcd(key[0], n) == 1:
+    if math.gcd(key[0], n) == 1:
+        if decryptCaesar (beginning_of_ciphertext, key) == beginning_of_plaintext: 
             possible_keys.append(key)   
 #print (possible_keys)
 
