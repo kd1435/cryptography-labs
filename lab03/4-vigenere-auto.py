9d0b9018c155715d23838369482b75c47dc2a604
kristis <kristisappdev@gmail.com>
2025-10-13 02:13:26 +0300

    Remove unnecessary code, remove key from ciphertext's head

--- lab03/4-vigenere-auto.py
+++ lab03/4-vigenere-auto.py
@@ -13,10 +13,6 @@ CSMJH YRVHC UOJLA CŽODŽ TJMUJ
 
 """
 
-# TODO: Fix typization
-
-from collections import defaultdict
-
 abc = 'AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ' # the alphabet
 n   = len (abc) # number of letters in the alphabet
 
@@ -56,69 +52,7 @@ def encryptVigenere (plaintext, key):
         cyphertext += number2letter ((letter2number (plaintext [i]) + keys [i % key_length]) % n)
     return cyphertext
 
-#print (encryptVigenere ('mountain', 'earth'))
-
-# performs the Friedman's test on the given text with the given shift
-def friedman_test (text, shift):
-    text = prepare (text)
-    text_length = len (text)
-    equal_letters_count = 0
-    for i in range (shift, text_length):
-        if text [i] == text [i - shift] :
-            equal_letters_count += 1
-    return  1. * equal_letters_count / (text_length - shift)
-
-#print (friedman_test ('ABDBSBSBSBBBCN', 2))
-
-# splits the given text into d parts
-def split (text, d):
-    text = prepare (text)
-    split_text = [''] * d # the list of d empty strings
-    text_length = len (text)
-    for i in range (text_length):
-        split_text [i % d] += text [i]
-    return split_text    
-    
-#print (split ('ABABABABABAB', 2))
-
-# orders the letters of the given text according to their frequency of appearance in it (most frequent first)
-def order_by_frequency (text):
-    text = prepare (text)
-    
-    # counts the frequencies of letters
-    frequencies = defaultdict (int)
-    for w in text:
-        frequencies [w] += 1                   
-
-    # sorts the letters most frequent first and puts them in the string ordered_letters
-    ordered_letters = ""
-    for w in sorted (frequencies, key=frequencies.get, reverse=True):
-        ordered_letters += w
-    return ordered_letters
-
-#print (order_by_frequency ('TRRYYYIIU'))
-
-# computes the part of the letters of test encrypted with the given key in the given ciphertext
-# test - most frequent letter string, key - guessed Caesar cipher key
-def guess(test, key, ciphertext): 
-    test = prepare (test)
-    ciphertext = prepare (ciphertext)
-    
-    # encrypts the given test with the given key
-    encrypted_test = ''
-    for r in test:
-        encrypted_test += number2letter ((letter2number (r) + key) % n)
-
-    # counts the letters of encrypted_test in the given ciphertext
-    nb = 0
-    for r in ciphertext:
-        if r in encrypted_test: 
-            nb += 1
-    return 1. * nb / len(ciphertext)
-
-#print (guess ('ABC', 2, 'ABABABASSDDHHDKKKCCCCCLKLDKSJJSJSJLL'))
-
-ciphertext = '''KNYGOS UŽKGG CUMKĄ VFŪŠU JUFNĘ AŲČJF 
+ciphertext = '''UŽKGG CUMKĄ VFŪŠU JUFNĘ AŲČJF 
 ĘIMZČ ŽĘDMŠ OASDC UŪJHŪ RURĖH 
 NJUBŲ DĘZYO ĘŲAGO HAĄPA ONJPD 
 EDŽCE LRDŪC SLFGS NLĘYM EĘAŲC 
@@ -129,44 +63,13 @@ CSMJH YRVHC UOJLA CŽODŽ TJMUJ
 ŲGINĘ BMĘIF FYMĄC PFDSĄ OCTDY'''
 key = 'KNYGOS'
 
-# inverse_key = ''.join([number2letter((-letter2number(k)) % n) for k in key])
-# print(encryptVigenere(ciphertext, inverse_key))
 
-plaintext = key
 key_length = len(key)
 
 ciphertext = prepare(ciphertext)
+# Assign key to plaintext as list, because plaintext will be mutable / appended to
+plaintext = list(key) 
 
-i = 0
 for letter in ciphertext:
-    text = 
-    plaintext.append(text)
-# print(friedman_test(ciphertext, 6)) 
-# d = 6
-
-# for i in range(15):
-#     print(friedman_test(ciphertext, i))
-
-# fragments = split(ciphertext, 6)
-# print(fragments)
-
-# for fragment in fragments:
-#     print(order_by_frequency(fragment))
-#     print()
-
-# test = 'IECR'
-
-
-# # for k in range(n):
-# #     print(k, number2letter(k), guess(test, k, fragments[3]))
-# # # key[3] = G ?
-
-# # for k in range(n):
-# #     print(k, number2letter(k), guess(test, k, fragments[4]))
-# # # key[4] = U ?
-
-# for k in range(n):
-#     print(k, number2letter(k), guess(test, k, fragments[5]))
-# # key[5] = S ?
-
-# solution: 
+    text = "?"
+    plaintext += text
