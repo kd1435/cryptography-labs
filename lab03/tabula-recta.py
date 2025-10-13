15325f847c60c9e091a77aa8af482786d830eba5
kristis <kristisappdev@gmail.com>
2025-10-13 04:30:01 +0300

    Remove tabula-recta.py, found one online for Lithuanian alphabet
    Was looking into autokey cipher decrypting tools online because
    The coded seemed okay based on the wikipedia's described concept of the cipher
    Seems like the exercise may be broke, the online tool which accepted Lithuanian alphabet gave the same output
    As in my code, which is a non-sensical plaintext

--- lab03/tabula-recta.py
+++
@@ -1,5 +0,0 @@
-# Create a Tabula recta https://en.wikipedia.org/wiki/Tabula_recta
-# For help with visualization / manual "eye" testing for solving exercise 4
-
-abc = 'AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ' # the alphabet
-n   = len (abc) # number of letters in the alphabet
