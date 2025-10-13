76fea5fa638b594c777665ad91c89e9be0c3e1aa
kristis <kristisappdev@gmail.com>
2025-10-13 03:11:54 +0300

    Add tabula recta, to-be-created, to help visualizing how exercise 4 may be solved

---
+++ lab03/tabula-recta.py
@@ -0,0 +1,5 @@
+# Create a Tabula recta https://en.wikipedia.org/wiki/Tabula_recta
+# For help with visualization / manual "eye" testing for solving exercise 4
+
+abc = 'AĄBCČDEĘĖFGHIĮYJKLMNOPRSŠTUŲŪVZŽ' # the alphabet
+n   = len (abc) # number of letters in the alphabet
